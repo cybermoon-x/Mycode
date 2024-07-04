07e966b2cb48a7723b0d1623e9e16a6a7d8ac0ce
cybermoon-x <cybertsuki8@gmail.com>
2024-07-04 21:32:19 +0000

    mix-list

---
+++ mix-list/mixlist01.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+#create list contiaing three items
+# my_list = [ "192.168.0.5", 5060, "UP" ]
+# display first item in the list
+# print("The first item in the list (IP): " + my_list[0] )
+# display second item in the list
+# print("The second item in the list (port): " + str(my_list[1]) )
+# display third item in the list
+# print("The last item in the list (state): " + my_list[2] )
+
+iplist = [5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh"]
+
+# display only ip addresses
+print(iplist[3:5])
+
